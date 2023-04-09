50569a3cc9da98b80ea76241d429e4e8f9fce5ec
Jordan0105 <jordanma2004@outlook.es>
2023-04-09 15:00:41 -0600

    Intro is done

--- components/Intro.jsx
+++ components/Intro.jsx
@@ -1,24 +1,39 @@
 import React from "react";
-import { Zoom } from "react-awesome-reveal";
-import { Fade } from "react-awesome-reveal";
-
+import { Zoom, Fade } from "react-awesome-reveal";
 const Intro = () => {
   return (
     <div className="intro">
       <div className="intro__name">
-        <Fade duration={2500}>
+        <Fade duration={2500} triggerOnce>
           <p className="intro__greeting">Hi, my name is...</p>
         </Fade>
-        <Zoom delay={1000}>
-          <h1 className="intro__my__name">Yordan Mejía.</h1>
+        <Zoom delay={1000} cascade triggerOnce>
+          <h1 className="intro__my__name">Yordan Mejía &</h1>
           <h2 className="intro__profession">I&#39;m a Front-End Developer</h2>
         </Zoom>
       </div>
-      <div id="intro__description">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Harum odio
-        quasi quas. Deleniti eligendi necessitatibus non rerum eius distinctio
-        corporis sint laboriosam, cupiditate perferendis unde similique sit.
-        Quibusdam, nobis praesentium.
+      <div className="intro__description">
+        <p>
+          <Fade cascade>
+            <span className="xd">
+              I&#39;m a System Engineering student with a strong passion for
+              learning about all things tech-related.
+            </span>
+            <span className="xd">
+              I love designing web pages and working on projects that challenge
+              me and help me grow.
+              <br />
+              <br />
+            </span>
+            <span className="xd">
+              Thanks for taking the time to read my introduction.
+            </span>
+            <span className="xd">
+              I&#39;m excited to connect with you and collaborate on stunnig and
+              innovative projects.
+            </span>
+          </Fade>
+        </p>
       </div>
     </div>
   );
