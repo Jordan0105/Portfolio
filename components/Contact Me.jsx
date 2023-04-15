f484e05c67b51d9c6892f60596ef84dbf9927caa
Jordan0105 <jordanma2004@outlook.es>
2023-04-14 23:27:56 -0600

    Contact Section was added

--- components/Contact Me.jsx
+++ components/Contact Me.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import "../src/css/css-components/Contact Me.css";
 
 function ContactMe() {
   return (
     <footer>
-      <div className="footer">Contact Me</div>
+      <div className="footer">
+        <nav className="menu__footer">
+          <ul className="menu__footer__list">
+            <li className="menu__footer__item">
+              <img
+                src="../assets/Icons/Email.png"
+                alt="E-mail"
+                className="menu__footer__icon"
+              />
+            </li>
+            <li className="menu__footer__item">
+              <img
+                src="../assets/Icons/Linkedin.png"
+                alt="E-mail"
+                className="menu__footer__icon"
+              />
+            </li>
+            <li className="menu__footer__item">
+              <img
+                src="../assets/Icons/GitHub.png"
+                alt="E-mail"
+                className="menu__footer__icon"
+              />
+            </li>
+            <li className="menu__footer__item">
+              <img
+                src="../assets/Icons/Twitter.png"
+                alt="Twitter"
+                className="menu__footer__icon"
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }
