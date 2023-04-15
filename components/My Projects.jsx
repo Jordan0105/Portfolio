ca11bde08d6d0f76eb2495f7ae4c3a5fda31810f
Jordan0105 <jordanma2004@outlook.es>
2023-04-14 22:41:21 -0600

    Some responsive improvements and Projects Cards are done

--- components/My Projects.jsx
+++ components/My Projects.jsx
@@ -50,6 +50,15 @@ function MyProjects() {
             alt="Countdown"
             className="project__list__item__image"
           />
+          <div className="project__list__card">
+            <h2 className="project__list__card__text">Countdown</h2>
+            <div className="project__list__info">
+              <h2 className="project__list__card__info__text">Countdown</h2>
+              <p className="project__list__card__info__description">
+                This is a Countdown built in with HTML, CSS, JavaScript & Bulma
+              </p>
+            </div>
+          </div>
         </div>
         <div className="projects__list__item">
           <img
@@ -57,6 +66,16 @@ function MyProjects() {
             alt="Amazon Clone"
             className="project__list__item__image"
           />
+          <div className="project__list__card">
+            <h2 className="project__list__card__text">Amazon Clone</h2>
+            <div className="project__list__info">
+              <h2 className="project__list__card__info__text">Amazon Clone</h2>
+              <p className="project__list__card__info__description">
+                This is a clone of Amazon website (UI) built it from scratch
+                just with HTML, CSS & JavaScript
+              </p>
+            </div>
+          </div>
         </div>
         <div className="projects__list__item">
           <img
@@ -64,6 +83,16 @@ function MyProjects() {
             alt="Calculator"
             className="project__list__item__image"
           />
+          <div className="project__list__card">
+            <h2 className="project__list__card__text">Calculator</h2>
+            <div className="project__list__info">
+              <h2 className="project__list__card__info__text">Calculator</h2>
+              <p className="project__list__card__info__description">
+                This is a basic calculator built it with HTML, CSS, JavaScript
+                and Bulma
+              </p>
+            </div>
+          </div>
         </div>
         <div className="projects__list__item">
           <img
@@ -71,13 +100,38 @@ function MyProjects() {
             alt="Heroes arent Heroes"
             className="project__list__item__image"
           />
+          <div className="project__list__card">
+            <h2 className="project__list__card__text">Heroes arent Heroes</h2>
+            <div className="project__list__info">
+              <h2 className="project__list__card__info__text">
+                Heroes arent Heroes
+              </h2>
+              <p className="project__list__card__info__description">
+                This is a basic game built it with HTML, CSS, JavaScript &
+                Rollup
+              </p>
+            </div>
+          </div>
         </div>
+
         <div className="projects__list__item">
           <img
             src="../assets/Icons/Veterinary Clinic 2.jpg"
             alt="Veterinary Clinic"
             className="project__list__item__image"
           />
+          <div className="project__list__card">
+            <h2 className="project__list__card__text">Veterinary Clinic</h2>
+            <div className="project__list__info">
+              <h2 className="project__list__card__info__text">
+                Veterinary Clinic
+              </h2>
+              <p className="project__list__card__info__description">
+                This is a Veterinary Clinic built it with React + Vite,
+                Bootstrap 5 & React Router
+              </p>
+            </div>
+          </div>
         </div>
         <div className="projects__list__item">
           <img
@@ -85,6 +139,15 @@ function MyProjects() {
             alt="Stopwatch"
             className="project__list__item__image"
           />
+          <div className="project__list__card">
+            <h2 className="project__list__card__text">My Portfolio</h2>
+            <div className="project__list__info">
+              <h2 className="project__list__card__info__text">My Portfolio</h2>
+              <p className="project__list__card__info__description">
+                This is my portfolio built it with React + Vite
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
